52f6b689bd1fd2d5572a8b58387b251274ce5acf
SublimeDevv <juanmen1404@gmail.com>
2024-02-23 07:31:06 -0500

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+    <title>SGE</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1 class="text-7xl text-red-500">Hola SGE</h1>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
