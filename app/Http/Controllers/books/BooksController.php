aeb3dbd17cf531bbcb9bf3ff073f2915875b152e
Gael Sanchez Lobato <22393182@utcancun.edu.mx>
2024-03-05 19:00:50 -0500

    Mejora a libros

--- app/Http/Controllers/books/BooksController.php
+++ app/Http/Controllers/books/BooksController.php
@@ -34,10 +34,29 @@ class BooksController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        // Aquí puedes obtener el libro específico con el ID proporcionado
+        $libros = [
+            ['imagen' => 'images/books/c-book.jpg', 'titulo' => "Programación orientada a objetos con C++ y Java.", "descripcion" => "Sumérgete en el mundo de la programación orientada a objetos con este libro, que cubre tanto C++ como Java, ofreciendo una guía completa desde los fundamentos hasta técnicas avanzadas, ideal para desarrolladores en busca de un dominio sólido en POO.", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/cert-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/java-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/java-poo-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/js-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/logica-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/logica-java-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/metodologia-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/model-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"],
+            ['imagen' => 'images/books/poo-book.jpg', 'titulo' => "Libro - $id", 'editorial' => "Editorial - $id"]
+        ];
+
+        $libro = $libros[$id];
+
+        // Luego, pasamos el libro a la vista de detalle del libro junto con el índice
+        return view('books-notifications.books.BookDetail', compact('libro', 'id'));
     }
+
+
 
     /**
      * Show the form for editing the specified resource.
--- resources/views/books-notifications/books/Books.blade.php
+++ resources/views/books-notifications/books/Books.blade.php
@@ -5,156 +5,39 @@
 @endsection
 
 @section('contenido')
+    @php
+    // Definir el arreglo de libros
+    $libros = [
+        ['imagen' => 'images/books/c-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/cert-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/java-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/java-poo-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/js-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/logica-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/logica-java-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/metodologia-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/model-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+        ['imagen' => 'images/books/poo-book.jpg', 'titulo' => 'Libro - ', 'editorial' => 'Editorial -'],
+    ];
+    @endphp
+
     <div class="container">
         <h1 class="title-books"> - Libros -</h1>
         <div class="flex flex-wrap mx-10 gap-10 ctn-bk">
             <div class=" w-full"> <!-- Establecemos una altura máxima que equivalga a mostrar 4 libros -->
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-                    <!-- Repite estas tarjetas según sea necesario -->
+                    <!-- Iterar sobre los elementos del arreglo -->
+                    @foreach($libros as $libro)
                     <div class="container-card-book">
                         <div class="card-book">
                             <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
+                                <img src="{{ asset($libro['imagen']) }}" alt="Imagen del libro">
                             </div>
                         </div>
-                        <button class="button-books"> Ver Libro</button>
+                       <a href="{{ route('books.show', ['book' => $loop->index]) }}"><button class="button-books">Ver Libro</button></a>
                     </div>
-                    <!-- Repite estas tarjetas según sea necesario -->
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-                    
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>
-
-                    <div class="container-card-book">
-                        <div class="card-book">
-                            <div class="card__content-book">
-                                <img src="{{ asset('images/books/js-book.jpg') }}" alt="Imagen del libro">
-                                <div>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="button-books"> Ver Libro</button>
-                    </div>  
-
+                    @endforeach
+                    <!-- Fin del bucle -->
                 </div>
             </div>
         </div>
