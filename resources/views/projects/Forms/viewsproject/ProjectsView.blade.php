4ddbbe2591fad6f3b84b95d36beb99541724fa01
Josue Antonio Chan Gutierrez <22393164@utcancun.edu.mx>
2024-03-06 13:52:26 -0500

    Agregando modificaciones

--- resources/views/projects/Forms/viewsproject/ProjectsView.blade.php
+++ resources/views/projects/Forms/viewsproject/ProjectsView.blade.php
@@ -1,67 +1,152 @@
 @extends('layouts.panel')
+@vite('resources/css/app.css')
 
-@section('titulo', 'Vista de Proyectos')
+@section('titulo', 'Proyectos por división')
 
 @section('contenido')
+    @for ($i = 0; $i < 5; $i++)
+        <div class="flex flex-col md:flex-row ml-16 mr-16 mb-10 bg-white mt-10 p-8 rounded-xl">
+            <div class="flex-none mb-4 md:mb-0 md:order-2 md:ml-10 md:mr-10">
+                <img class="mt-7 cursor-pointer w-96 md:w-96 md:h-72 h-auto p-1 rounded-2xl"
+                    src={{ asset('images/divisions/ing.png') }} alt="Project Image">
 
-<div class="project-container">
-    <!-- Imagen del Proyecto -->
-    <div class="project-image">
-        <img src="{{ asset('images/divisions/ec.png') }}" alt="Imagen del Proyecto">
-    </div>
-    <!-- Project Header -->
-    <div class="project-header">
-        <h2>Nombre del Proyecto</h2>
-        <div class="container-state">
-            <p>Estado del proyecto:</p>
-            <h1>Aprobado</h1>
-        </div>
-    </div>
-    <!-- Project Body -->
-    <div class="project-body">
-        <p>Descripción</p>
-        <div class="project-description">
-            Josue Antonio Chan Gutierrez
-        </div>
+            </div>
 
-        <!-- Reaction buttons -->
-        <div class="project-reactions">
-            <button id="like-btn" class="unfilled">&#x2764;</button> <!-- Heart emoji -->
-            <button id="star-btn">&#x2B50;</button> <!-- Star emoji -->
-            <button id="comment-btn">&#x1F4AC;</button> <!-- Comment emoji -->
-        </div>
-    </div>
-</div>
 
-<!-- Modal para Comentarios - Fuera del .project-container -->
-<div id="commentModal" class="modal">
-    <!-- Contenido del modal -->
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <div class="modal-header">
-            <img src="{{ asset('images/divisions/ts-profile.png') }}" alt="Avatar" class="avatar">
-            <h2>Rafael Villegas Velasco</h2>
-        </div>
-        <textarea placeholder="Escribe un comentario..."></textarea>
-        <div class="modal-footer">
-            <button class="resolve-btn">Enviar</button>
-        </div>
-    </div>
-</div>
 
-<!-- Modal de Clasificación - Fuera del .project-container -->
-<div id="starModal" class="star-modal">
-    <!-- Contenido del modal -->
-    <div class="star-modal-content">
-        <span class="close-star">&times;</span>
-        <h3>Calificación</h3>
-        <!-- Contenedor de estrellas -->
-        <div class="star-rating">
-            @for ($i = 1; $i <= 5; $i++)
-                <span class="star" data-value="{{ $i }}">&#9733;</span> <!-- Estrella -->
-            @endfor
+            <div class="flex-grow ml-4 md:ml-0">
+                <div class="flex items-center">
+                    <i class='bx bxs-badge-check mr-3 text-[#03A696] text-3xl'></i>
+                    <h1 class="text-3xl font-bold text-gray-600">Niux - Tienda en línea</h1>
+                </div>
+
+
+                <a class="text-blue-500 hover:underline inline-flex items-center">
+                </a>
+                <h2 class="mb-4 text-gray-500 dark:text-gray-500"> Este proyecto es una plataforma innovadora diseñada para
+                    revolucionar la forma en que interactuamos con el mundo digital. Utilizando tecnología de punta,
+                    incluida la inteligencia artificial, el aprendizaje automático y la computación en la nube, busca
+                    proporcionar soluciones personalizadas a problemas comunes en la industria de la tecnología...
+
+                </h2>
+                <div class="flex items-center">
+                    <i class='bx bxs-group mt-[-12px] mr-3 text-[#03A696]'></i>
+                    <h2 class="mb-4 text-gray-500 dark:text-gray-600 font-bold">Integrantes</h2>
+                </div>
+
+                <div>
+                    <div class="flex -space-x-2 ">
+                        <div class="group hs-tooltip inline-block">
+                            <img class="hs-tooltip-toggle relative inline-block size-[46px] rounded-full ring-2 ring-white hover:z-10"
+                                src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                                alt="Image Description">
+                            <span
+                                class="opacity-0 invisible group-hover:opacity-100 group-hover:visible hs-tooltip-content absolute z-10 py-1.5 px-2.5 bg-gray-900 text-xs text-white rounded-lg dark:bg-neutral-700 transition-opacity duration-300"
+                                role="tooltip">
+                                James
+                            </span>
+                        </div>
+                        <div class="group hs-tooltip inline-block">
+                            <img class="hs-tooltip-toggle relative inline-block size-[46px] rounded-full ring-2 ring-white hover:z-10"
+                                src="https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                                alt="Image Description">
+                            <span
+                                class="opacity-0 invisible group-hover:opacity-100 group-hover:visible hs-tooltip-content absolute z-10 py-1.5 px-2.5 bg-gray-900 text-xs text-white rounded-lg dark:bg-neutral-700 transition-opacity duration-300"
+                                role="tooltip">
+                                María
+                            </span>
+                        </div>
+                        <div class="group hs-tooltip inline-block">
+                            <img class="hs-tooltip-toggle relative inline-block size-[46px] rounded-full ring-2 ring-white hover:z-10"
+                                src="https://images.unsplash.com/photo-1541101767792-f9b2b1c4f127?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&&auto=format&fit=facearea&facepad=3&w=300&h=300&q=80"
+                                alt="Image Description">
+                            <span
+                                class="opacity-0 invisible group-hover:opacity-100 group-hover:visible hs-tooltip-content absolute z-10 py-1.5 px-2.5 bg-gray-900 text-xs text-white rounded-lg dark:bg-neutral-700 transition-opacity duration-300"
+                                role="tooltip">
+                                Anna
+                            </span>
+                        </div>
+                        <div class="group hs-tooltip inline-block">
+                            <img class="hs-tooltip-toggle relative inline-block size-[46px] rounded-full ring-2 ring-white hover:z-10"
+                                src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                                alt="Image Description">
+                            <span
+                                class="opacity-0 invisible group-hover:opacity-100 group-hover:visible hs-tooltip-content absolute z-10 py-1.5 px-2.5 bg-gray-900 text-xs text-white rounded-lg dark:bg-neutral-700 transition-opacity duration-300"
+                                role="tooltip">
+                                Brian
+                            </span>
+                        </div>
+                        <div class="like-dislike-container">
+                            <div class="icons-box">
+                                <div class="icons">
+                                    <label class="btn-label" for="like-checkbox">
+                                        <span class="like-text-content">2</span>
+                                        <input class="input-box" id="like-checkbox" type="checkbox">
+                                        <svg class="svgs" id="icon-like-solid" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 512 512">
+                                            <path
+                                                d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z">
+                                            </path>
+                                        </svg>
+                                        <svg class="svgs" id="icon-like-regular" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 512 512">
+                                            <path
+                                                d="M323.8 34.8c-38.2-10.9-78.1 11.2-89 49.4l-5.7 20c-3.7 13-10.4 25-19.5 35l-51.3 56.4c-8.9 9.8-8.2 25 1.6 33.9s25 8.2 33.9-1.6l51.3-56.4c14.1-15.5 24.4-34 30.1-54.1l5.7-20c3.6-12.7 16.9-20.1 29.7-16.5s20.1 16.9 16.5 29.7l-5.7 20c-5.7 19.9-14.7 38.7-26.6 55.5c-5.2 7.3-5.8 16.9-1.7 24.9s12.3 13 21.3 13L448 224c8.8 0 16 7.2 16 16c0 6.8-4.3 12.7-10.4 15c-7.4 2.8-13 9-14.9 16.7s.1 15.8 5.3 21.7c2.5 2.8 4 6.5 4 10.6c0 7.8-5.6 14.3-13 15.7c-8.2 1.6-15.1 7.3-18 15.1s-1.6 16.7 3.6 23.3c2.1 2.7 3.4 6.1 3.4 9.9c0 6.7-4.2 12.6-10.2 14.9c-11.5 4.5-17.7 16.9-14.4 28.8c.4 1.3 .6 2.8 .6 4.3c0 8.8-7.2 16-16 16H286.5c-12.6 0-25-3.7-35.5-10.7l-61.7-41.1c-11-7.4-25.9-4.4-33.3 6.7s-4.4 25.9 6.7 33.3l61.7 41.1c18.4 12.3 40 18.8 62.1 18.8H384c34.7 0 62.9-27.6 64-62c14.6-11.7 24-29.7 24-50c0-4.5-.5-8.8-1.3-13c15.4-11.7 25.3-30.2 25.3-51c0-6.5-1-12.8-2.8-18.7C504.8 273.7 512 257.7 512 240c0-35.3-28.6-64-64-64l-92.3 0c4.7-10.4 8.7-21.2 11.8-32.2l5.7-20c10.9-38.2-11.2-78.1-49.4-89zM32 192c-17.7 0-32 14.3-32 32V448c0 17.7 14.3 32 32 32H96c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32z">
+                                            </path>
+                                        </svg>
+                                        <div class="fireworks">
+                                            <div class="checked-like-fx"></div>
+                                        </div>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="rating">
+                            <input type="radio" id="star-1" name="star-radio" value="star-1">
+                            <label for="star-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path pathLength="360"
+                                        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <input type="radio" id="star-2" name="star-radio" value="star-1">
+                            <label for="star-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path pathLength="360"
+                                        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <input type="radio" id="star-3" name="star-radio" value="star-1">
+                            <label for="star-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path pathLength="360"
+                                        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <input type="radio" id="star-4" name="star-radio" value="star-1">
+                            <label for="star-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path pathLength="360"
+                                        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <input type="radio" id="star-5" name="star-radio" value="star-1">
+                            <label for="star-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path pathLength="360"
+                                        d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z">
+                                    </path>
+                                </svg>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    @endfor
 
 @endsection
