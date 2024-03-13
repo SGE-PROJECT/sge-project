925a0b6793b29ce3394fcd4e257851767f9802f1
Anthony <anthowill1608@gmail.com>
2024-03-13 11:41:56 -0500

    Se modificaron los componentes de las graficas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ export default {
       "./resources/**/*.vue",
     ],
     theme: {
+        borderWidth: {
+            DEFAULT: '1px',
+            '0': '0',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '6': '6px',
+            '8': '8px',
+            '16': '16px',
+        },
+
       extend: {
+        colors: {
+            'green-ut': '#03a696',
+          },
+
         padding: {
             '50p': '50%',
             '30p': '30%',
