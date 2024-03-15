d664f65ee73e654ee161cea8dcbca902fdbfdc9f
Josue Antonio Chan Gutierrez <22393164@utcancun.edu.mx>
2024-03-15 07:20:34 -0500

    Agregando funcionalidad proyectos

---
+++ database/migrations/2024_03_15_013556_create_project_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('project_students')) {
+            Schema::disableForeignKeyConstraints();
+            Schema::create('project_students', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('id_project_id')->constrained('projects')->onDelete('restrict');
+                $table->foreignId('id_student_id')->constrained('students')->onDelete('restrict');
+                $table->boolean('is_main_student')->default(null );
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_students');
+    }
+};
