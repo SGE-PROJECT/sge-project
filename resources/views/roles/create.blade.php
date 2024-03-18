be6657fbd6880e350b49c54b6f2b75d53da8be18
JoseMtz02 <josemtzlara@hotmail.com>
2024-03-18 18:46:25 -0500

    se agrega diseño a las vistas crud de roles

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -5,61 +5,60 @@
 @endsection
 
 @section('contenido')
-<link rel="stylesheet" href="/css/roles/crateroles.css">
+    <link rel="stylesheet" href="/css/roles/crateroles.css">
 
-<div class="container">
-    <div class="heading">
-        {{ __('Crear Nuevo Rol') }}
-    </div>
+    <div class="container">
+        <div class="heading">
+            {{ __('CREAR NUEVO ROL') }}
+        </div>
 
-    <div class="edit-form">
-        <form method="POST" action="{{ route('roles.permissions.store') }}">
-            @csrf
+        <div class="edit-form">
+            <form method="POST" action="{{ route('roles.permissions.store') }}">
+                @csrf
+                <div class="form-group-name">
+                    <div class="input-box">
+                        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus />
+                        <label for="name">Nombre del rol</label>
 
-            <div class="form-group">
-                <label for="name" class="label">{{ __('Nombre del Rol') }}</label>
-
-                <div>
-                    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus class="input">
-
-                    @error('name')
-                        <span role="alert" class="error-message">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                        @error('name')
+                            <span role="alert" class="error-message">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label class="label">{{ __('Permisos') }}</label>
+                <div class="form-group">
+                    <label class="permission-label">{{ __('Permisos') }}</label>
 
-                <div class="checkbox-group">
-                    @foreach($permissions as $permission)
-                        <div class="checkbox">
-                            <input type="checkbox" value="{{ $permission->name }}" name="permissions[]" id="permission{{ $permission->id }}" class="input-checkbox">
-                            <label for="permission{{ $permission->id }}" class="checkbox-label">
-                                {{ $permission->description }}
-                            </label>
-                        </div>
-                    @endforeach
+                    <div class="checkbox-group">
+                        @foreach ($permissions as $permission)
+                            <div class="checkbox">
+                                <input type="checkbox" value="{{ $permission->name }}" name="permissions[]"
+                                    id="permission{{ $permission->id }}" class="input-checkbox">
+                                <label for="permission{{ $permission->id }}" class="checkbox-label">
+                                    {{ $permission->description }}
+                                </label>
+                            </div>
+                        @endforeach
 
-                    @error('permissions')
-                        <span role="alert" class="error-message">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                        @error('permissions')
+                            <span role="alert" class="error-message">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Crear Rol') }}
-                </button>
-                <a href="{{ route('roles.permissions.index') }}" class="btn btn-secondary">
-                    {{ __('Cancelar') }}
-                </a>
-            </div>
-        </form>
+                <div class="botones-container">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('AGREGAR ROL') }}
+                    </button>
+                    <a href="{{ route('roles.permissions.index') }}" class="btn btn-secondary">
+                        {{ __('CANCELAR') }}
+                    </a>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
