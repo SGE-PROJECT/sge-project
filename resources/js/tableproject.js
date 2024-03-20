e7897224da81701d818d582a386e10a2129da9d6
irving <martinezlopezirving562@gmail.com>
2024-03-20 10:57:49 -0600

    Funcionalidad de la gráfica lista

--- resources/js/tableproject.js
+++ resources/js/tableproject.js
@@ -53,38 +53,3 @@ $(document).ready(function () {
         }
     });
 });
-
-
-var ctx = document.getElementById('barChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['En desarrollo', 'Reprobados', 'Completados',],
-        datasets: [{
-            label: 'Estado del proyecto',
-            data: [$enDesarrolloCount, 4, 10], 
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
--- resources/views/administrator/project.blade.php
+++ resources/views/administrator/project.blade.php
@@ -1,10 +1,11 @@
 <!-- SECCION PROYECTOS -->
 @extends('layouts.panel')
+@section('titulo', 'Proyectos')
 @section('contenido')
     <h1 class="text-3xl font-bold text-center mt-5">Proyectos</h1>
     <!-- SECCIÓN QUE CONTIENE LA TARJETA Y LA GRÁFICA -->
     <div class="flex flex-wrap justify-center gap-5 p-5">
-        <div class="p-6 grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
+        <div class="p-6 grid sm:grid-cols-2 lg:grid-cols-2 gap-5">
             @include('administrator.graph-projects')
             @include('administrator.graph-users')
         </div>
@@ -176,6 +177,44 @@
     </div>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="{{ asset('js/tableproject.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+        var ctx = document.getElementById('barChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['En desarrollo', 'Reprobados', 'Completados'],
+                datasets: [{
+                    label: 'Estado del proyecto',
+                    data: [
+                        {{ $enDesarrolloCount }},
+                        {{ $reprobadosCount }},
+                        {{ $completadosCount }}
+                    ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    });
+    </script>
 @endsection
