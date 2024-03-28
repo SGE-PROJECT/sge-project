0577f8448e8d13a91bf86ac7db403a6af926f485
irving <martinezlopezirving562@gmail.com>
2024-03-27 20:17:08 -0600

    Solución error en las gráficas

--- resources/views/administrator/graph-projects.blade.php
+++ resources/views/administrator/graph-projects.blade.php
@@ -62,9 +62,9 @@
             name: 'Datos',
             innerSize: '60%',
             data: [
-            { name: 'Completados', y: {{$completadosCount}}, color: '#22C55E' },
-            { name: 'En desarrollo', y: {{$enDesarrolloCount}}, color: '#eab308' },
-            { name: 'Reprobados', y: {{$reprobadosCount}}, color: '#9ca3af' },
+            { name: 'Completados', y:19, color: '#22C55E' },
+            { name: 'En desarrollo', y: 13, color: '#eab308' },
+            { name: 'Reprobados', y: 4, color: '#9ca3af' },
             ]
         }]
     });
