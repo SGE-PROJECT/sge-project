71d68fede4ed333517b43055544b58f4783cf023
Garcia Canul Guillermo de Jesus <22393204@utcancun.edu.mx>
2024-04-04 11:31:33 -0500

    Se mejora formato de Export para usuarios

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -3,15 +3,48 @@
 namespace App\Exports;
 
 use App\Models\User;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class UserExport implements FromCollection
+class UserExport implements FromCollection, WithHeadings, WithMapping
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    use \Maatwebsite\Excel\Concerns\Exportable;
+
     public function collection()
     {
-        return User::all();
+        // Cargar previamente la relación roles para optimizar la consulta
+        return User::with('roles')->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Name',
+            'Email',
+            'Identifier Number',
+            'Roles', // Cambiado de 'Role' a 'Roles' para reflejar que puede haber múltiples
+            'Division ID',
+            'Phone Number',
+            'Avatar',
+            'Is Active',
+            // Agrega más cabeceras según sea necesario
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->name,
+            $user->email,
+            $user->identifier_number,
+            implode(', ', $user->roles->pluck('name')->toArray()), // Modificado para usar la relación cargada y unir todos los nombres de roles
+            $user->division_id,
+            $user->phone_number,
+            $user->avatar,
+            $user->isActive ? 'Yes' : 'No',
+        ];
     }
 }
