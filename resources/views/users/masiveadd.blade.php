aded0a13024eb727bb6eda0ea65d41df83c9641d
Garcia Canul Guillermo de Jesus <22393204@utcancun.edu.mx>
2024-04-03 21:49:26 -0500

    Funcion para masivo

--- resources/views/users/masiveadd.blade.php
+++ resources/views/users/masiveadd.blade.php
@@ -7,12 +7,8 @@ Gestión Masiva De Usuarios
 @section('contenido')
 <h1 class="text-center font-bold text-4xl py-4 text-black">Registro de usuarios</h1>
 
-<a  href="{{ route('users.masiveadd.export')}}" class="Btn_divisions modal-button ml-8 p-2.5 bg-teal-500 text-white  rounded hover:bg-teal-600 transition-colors">Descargar Excell</a>
-<form action="{{ route('users.masiveadd.import') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file" required>
-    <button type="submit" class="Btn_divisions modal-button ml-8 p-2.5 bg-teal-500 text-white rounded hover:bg-teal-600 transition-colors">Subir Excel</button>
-</form>
+<a  href="{{ route('masiveadd.exportCsv')}}" class="Btn_divisions modal-button ml-8 p-2.5 bg-teal-500 text-white  rounded hover:bg-teal-600 transition-colors">Descargar Excell</a>
+<a  href="{{ route('users.masiveadd.import')}}" class="Btn_divisions modal-button ml-8 p-2.5 bg-teal-500 text-white  rounded hover:bg-teal-600 transition-colors">Subir Excell</a>
 
 <div class="tabla-project rounded-t-lg">
     <div class="tabla-cont-project rounded-t-lg">
--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,7 @@ Route::middleware(['auth'])->group(function () {
 Route::post('gestion-usuarios-masiva/import', [MasiveAddController::class, 'import'])->name('users.masiveadd.import');
 
 // Ruta adicional para la exportación de la plantilla de usuarios
-Route::get('gestion-usuarios-masiva/export', [MasiveAddController::class, 'export'])->name('users.masiveadd.export');
+Route::get('gestion-usuarios-masiva/export-csv', [MasiveAddController::class, 'exportCsv'])->name('masiveadd.exportCsv');
 
     //Inicia Modulo de Divisiones, Empresas y Carreras conjuntas en proyectos por division.
 
