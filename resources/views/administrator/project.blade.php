6e350038d091144cd890243491a9bdd6e6d69f99
irving <martinezlopezirving562@gmail.com>
2024-04-08 23:22:10 -0600

    Se añade Data Tables en la vista Proyectos

--- resources/views/administrator/project.blade.php
+++ resources/views/administrator/project.blade.php
@@ -4,27 +4,27 @@
 @section('contenido')
 
     <h1 class="text-3xl font-bold text-center mt-5">Proyectos</h1>
-    <!-- SECCIÓN QUE CONTIENE LA TARJETA Y LA GRÁFICA -->
-
-        <div class="p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
-            @include('administrator.graphs.graph-anteprojects')
-            @include('administrator.graphs.graph-projects')
-            @include('administrator.graphs.graph-users')
-        </div>
-        <div class="p-6 grid sm:grid-cols-1 lg:grid-cols-2 gap-5">
-            <!-- Gráfica de barras a la izquierda -->
-            <div class="flex flex-col lg:flex-row items-stretch w-full lg:w-auto">
-                <div id="barChartContainer"
-                    class="seccion-projects p-12 relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 w-full shadow-lg rounded-xl font-sans">
-                    <canvas id="barChart" class="mt-5"
-                        style="display: block; box-sizing: border-box; height: 300px; width: 400px;"></canvas>
-                </div>
-            </div>
-            <!-- Componente administrator.section-projects a la derecha -->
-            <div class="flex flex-col lg:flex-row items-stretch gap-5 w-full">
-                @include('administrator.section-projects')
+    <!-- SECCIÓN QUE CONTIENE LAS TARJETAS Y LAS GRÁFICAS -->
+    <div class="p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <!-- Tarjetas -->
+        @include('administrator.graphs.graph-anteprojects')
+        @include('administrator.graphs.graph-projects')
+        @include('administrator.graphs.graph-users')
+    </div>
+        <!-- Gráfica 1 -->
+    <div class="p-6 grid sm:grid-cols-1 lg:grid-cols-2 gap-5">
+        <div class="flex flex-col lg:flex-row items-stretch w-full lg:w-auto">
+            <div id="barChartContainer"
+                class="seccion-projects p-12 relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 w-full shadow-lg rounded-xl font-sans">
+                <canvas id="barChart" class="mt-5"
+                    style="display: block; box-sizing: border-box; height: 300px; width: 400px;"></canvas>
             </div>
         </div>
+        <!-- Gráfica 2 -->
+        <div class="flex flex-col lg:flex-row items-stretch gap-5 w-full">
+            @include('administrator.section-projects')
+        </div>
+    </div>
 
     <!-- SECCIÓN QUE CONTIENE LOS BOTONES -->
     <div class="flex items-end align-middle">
@@ -153,7 +153,7 @@
     <!-- CONTENEDOR DE LA TABLA -->
     <div id="tabla-container" class="tabla-project rounded-t-lg">
         <div class="tabla-cont-project rounded-t-lg">
-            <table id="tabla-proyectos">
+            <table id="tabla-proyectos" class="display" style="width:100%">
                 <thead>
                     <tr>
                         <th>Proyecto</th>
@@ -180,10 +180,20 @@
                 </tbody>
             </table>
         </div>
-        <!-- CONTENEDOR DE LA PAGINACIÓN -->
     </div>
+    <!-- SCRIPTS DE JQUERY -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- SCRIPTS DE DATA TABLES Y DATA TABLE BUTTONS -->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
+    <!-- SCRIPTS PARA HACER FUNCIONAR LOS BOTONES -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.print.min.js"></script>
+    <!-- SCRIPTS PARA LA GRÁFICA -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="{{ asset('js/tableproject.js') }}"></script>
     <script>
@@ -219,6 +229,50 @@
                             beginAtZero: true
                         }
                     }
+                }
+            });
+        });
+    </script>
+    <!-- SCRIPT DE LA DATA TABLE -->
+    <script>
+        $(document).ready(function() {
+            $('#tabla-proyectos').DataTable({
+                pageLength: 25,
+                responsive: true,
+                dom: 'Bfrtip',
+                buttons: [{
+                        extend: 'excelHtml5',
+                        text: '<i class="fas fa-file-excel"></i> ',
+                        titleAttr: 'Exportar a Excel',
+                        className: 'btn btn-success'
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        text: '<i class="fas fa-file-pdf"></i> ',
+                        titleAttr: 'Exportar a PDF',
+                        className: 'btn btn-danger'
+                    },
+                    {
+                        extend: 'print',
+                        text: '<i class="fa fa-print"></i> ',
+                        titleAttr: 'Imprimir',
+                        className: 'btn btn-info'
+                    }
+                ],
+                language: {
+                    "lengthMenu": "Mostrar _MENU_ registros",
+                    "zeroRecords": "No se encontraron resultados",
+                    "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "sSearch": "Buscar:",
+                    "oPaginate": {
+                        "sFirst": "Primero",
+                        "sLast": "Último",
+                        "sNext": "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "sProcessing": "Procesando...",
                 }
             });
         });
