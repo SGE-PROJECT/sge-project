f313297a86909477bcbdce8c0b0660ce743f1d90
Diaz Rosales Gerardo Antonio <22393168@utcancun.edu.mx>
2024-04-10 17:08:34 -0500

    Commit prueba antes de subir cambios

--- routes/web.php
+++ routes/web.php
@@ -154,7 +154,7 @@ Route::get('/scraping',[BooksController::class, 'imageBooks']);
     Route::get('/proyectos', [ProjectController::class, 'list'])->name('Proyectos');
     Route::get('/', [ProjectController::class, 'dashgeneral'])->name('Dashboard-Proyectos');
     Route::get('/anteproyectos', [ProjectController::class, 'dashAnteprojects'])->name('Dashboard-Anteproyectos');
-    Route::get('proyectoinvitacion', [ProjectController::class, 'invitation']);
+    Route::get('proyectoinvitacion', [ProjectController::class, 'invitation'])->name('projectinvitation');
     Route::get('formanteproyecto', [ProjectController::class, 'projectform'])->name('projectform');
     Route::post('formanteproyecto', [ProjectController::class, 'store'])->name('envproyecto');
     Route::resource('projects', ProjectController::class);
