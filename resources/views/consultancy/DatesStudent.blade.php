bacdac75b83ef6a2f2ace0c0ddab94a25069b321
Alonso <carlosroano201@gmail.com>
2024-04-12 11:29:44 +0000

    Sanciones usuario

--- resources/views/consultancy/DatesStudent.blade.php
+++ resources/views/consultancy/DatesStudent.blade.php
@@ -16,10 +16,28 @@
     @endif
     <main class="vista_asesorias">
         @if (!$advisor)
-            <h2 style="color: #293846; font-size: 24px; font-weight: bold; text-align: left; width: 80%; margin-top:50px;">
-                Opss... parece que aun no tienes un asesor designado...</h2>
-            <h2 style="color: #293846; font-size: 24px; font-weight: bold; text-align: left; width: 80%;">Lo sentimos, puedes
-                contactar con servicios estudiantiles para que se te asigne un asesor, numero: 235 103 5581.</h2>
+        <div
+        class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+        <div class="xl:pt-2 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+            <div class="relative">
+                <div class="absolute">
+                    <div class="">
+                        <h1 class="my-2 text-gray-800 font-bold text-2xl">
+                            ¡Aún no cuentas con un asesor académico!
+                        </h1>
+                        <p class="my-2 text-gray-800">
+                            No te preocupes, pronto te asignarán uno para comenzar con tu anteproyecto.
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div>
+            <img class="w-96" src="Icons/graduacion.png" />
+        </div>
+    </div>
         @endif
 
 
