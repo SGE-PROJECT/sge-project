47772d074dc152a0729d31e51829d86003a50b48
Alonso <carlosroano201@gmail.com>
2024-04-14 03:45:31 +0000

    Reporte de sanciones

---
+++ resources/views/mails/sactionAdvisor.blade.php
@@ -0,0 +1,7 @@
+<h1>¡Hola, <span style="color:#00ab84">{{ $users->name }}</span>!</h1>
+
+<p>Haz aplicado una sancion a tu asesorado.</p>
+
+<p><span style="color:#00ab84">Debido al siguiente motivo:</span></p>
+<p> {{ $mensaje }}</p>
+<p>A continuacion se te adjunta una copia del documento que tu asesorado deberá imprimir y presentar ante usted. Esto en caso de no presentarse el documento de parte de tu asesorado y asi poder aplicar la sancion de una manera adecuada.</p>
